c7640da36a2346c9f03e8dd85c9953f74e8417d2
mzator <mzator93@gmail.com>
2016-11-12 22:02:30 +0100

    Dodanie

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoCentrum.Startup))]
+namespace AutoCentrum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
